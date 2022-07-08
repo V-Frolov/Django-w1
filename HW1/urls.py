ed88e7837ec8decba0436693e41ba9846ed60c91
Vitaliy Frolov <frolov.vitaliy@gmail.com>
2022-07-08 16:58:58 +0300

    Task 1 completed

--- HW1/urls.py
+++ HW1/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse, HttpRequest
-
-def hellodjango(request: HttpRequest):
-    return HttpResponse("Hello Django!")
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hellodjango)
+    path('', include('firstapp.urls'))
 ]
